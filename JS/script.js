58cd74aae7ba18c315a8e604fe2101e6ea8a35e8
Ahmed Elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-06-14 17:52:23 +0300

    added validation to login page

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,51 @@
 var button = document.getElementsByTagName("button")[0];
 var form = document.getElementsByTagName("form")[0];
-
+var all = document.getElementById("all");
+var emailval = document.getElementById("emailval");
+var passval = document.getElementById("passval");
+var confpassval = document.getElementById("confpassval");
+var emailPattern =
+  /^[a-zA-Z][a-zA-Z0-9]{0,9}[\.][a-zA-Z0-9]{0,15}@[a-z]{2,9}[\.][a-z]{3,9}$/;
+var passwordPattern = /^(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   var userName = document.getElementById("username");
+  var email = document.getElementById("email");
   var userPassword = document.getElementById("password");
-  console.log(userName.value);
-  console.log(userPassword.value);
-  document.cookie = `username=${userName.value}; `;
-  document.cookie = `password=${userPassword.value}; `;
-  window.location.href = "./home.html";
+  var confpassword = document.getElementById("confpassword");
+  var valid = IsValida(userName, email, userPassword, confpassword);
+  if (valid) {
+    document.cookie = `name=${userName.value}; `;
+    document.cookie = `password=${userPassword.value}; `;
+    document.cookie = `email=${email.value}; `;
+    window.location.href = "./home.html";
+  }
 });
+
+function IsValida(userName, email, password, confpassword) {
+  if (
+    name.value == "" ||
+    userName.value == "" ||
+    email.value == "" ||
+    password.value == "" ||
+    confpassword.value == ""
+  ) {
+    all.style.display = "block";
+    return false;
+  } else if (!emailPattern.test(email)) {
+    emailval.style.display = "block";
+    return false;
+  } else if (!passwordPattern.test(password)) {
+    passval.style.display = "block";
+    return false;
+  } else if (password != confpassword) {
+    confpassval.style.display = "block";
+    return false;
+  } else {
+    all.style.display = "none";
+    emailval.style.display = "none";
+    passval.style.display = "none";
+    confpassval.style.display = "none";
+    return true;
+  }
+}
