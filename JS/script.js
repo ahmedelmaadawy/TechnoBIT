3a38a525fde5328fa2faa76dc037ce2f17b174ef
Ahmed Elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-06-22 14:54:56 +0300

    update index page

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 var button = document.getElementsByTagName("button")[0];
 var form = document.getElementsByTagName("form")[0];
 var all = document.getElementById("all");
+var nameval = document.getElementById("nameval");
 var emailval = document.getElementById("emailval");
 var passval = document.getElementById("passval");
 var confpassval = document.getElementById("confpassval");
@@ -39,18 +40,27 @@ function IsValida(userName, email, password, confpassword) {
   ) {
     all.style.display = "block";
     return false;
+  } else if (!isNaN(parseInt(userName))) {
+    nameval.style.display = "block";
+    all.style.display = "none";
   } else if (!emailPattern.test(email)) {
     all.style.display = "none";
+    nameval.style.display = "none";
+
     emailval.style.display = "block";
     return false;
   } else if (!passwordPattern.test(password)) {
     emailval.style.display = "none";
+    nameval.style.display = "none";
+
     all.style.display = "none";
     passval.style.display = "block";
     return false;
   } else if (password != confpassword) {
     emailval.style.display = "none";
     all.style.display = "none";
+    nameval.style.display = "none";
+
     passval.style.display = "none";
     confpassval.style.display = "block";
     return false;
@@ -59,6 +69,7 @@ function IsValida(userName, email, password, confpassword) {
     emailval.style.display = "none";
     passval.style.display = "none";
     confpassval.style.display = "none";
+    nameval.style.display = "none";
     return true;
   }
 }
